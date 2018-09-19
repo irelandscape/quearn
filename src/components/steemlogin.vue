3f8b4578a37f85531f266f7434083c6195816201
Olivier Deme <olivier.deme@gmail.com>
2018-09-19 20:47:20 +0100

    Removed right drawer

--- src/components/steemlogin.vue
+++ src/components/steemlogin.vue
@@ -4,60 +4,10 @@
       :avatar="avatar"
       color="primary"
       square
-      @click="rightDrawerOpen = !rightDrawerOpen"
       class="nobackground"
     >
       {{username}}
     </q-chip>
-
-    <q-layout-drawer
-      v-model="rightDrawerOpen"
-      side = "right"
-      :overlay=true
-      :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
-    >
-      <q-list
-        no-border
-        link
-        inset-delimiter
-      >
-        <q-list-header>My Account</q-list-header>
-        <q-item @click.native="openURL('http://quasar-framework.org')">
-          <q-item-side icon="account_box" />
-          <q-item-main :label="$tc('myaccount')" />
-        </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="bookmark" />
-          <q-item-main :label="$tc('bookmark', 2)" />
-        </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="contact_support" />
-          <q-item-main :label="$t('myquestions')" />
-        </q-item>
-
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="check_circle" />
-          <q-item-main :label="$t('myanswers')" />
-        </q-item>
-
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="message" />
-          <q-item-main :label="$t('mycomments')" />
-        </q-item>
-        <q-item @click.native="myTopics">
-          <q-item-side icon="assignment" />
-          <q-item-main :label="$t('mytopics')" />
-        </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
-        </q-item>
-        <q-item @click.native="logout()">
-          <q-item-side icon="exit_to_app" />
-          <q-item-main :label="$t('logout')" />
-        </q-item>
-      </q-list>
-    </q-layout-drawer>
   </div>
   <div v-else>
     <q-btn
@@ -74,8 +24,7 @@ export default {
   name: 'Steemlogin',
   data: function () {
     return {
-      client: null,
-      rightDrawerOpen: this.$q.platform.is.desktop
+      client: null
     }
   },
   props: {
@@ -125,8 +74,6 @@ export default {
 
 <style lang="stylus" scoped>
   @import '~variables'
-  .q-drawer-container
-    color $primary
 
   >>> .nobackground
     background: none !important;
